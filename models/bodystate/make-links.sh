9e4d711f0c99392c4f12c755747f0cbac06ba180
Miguel Munoz <mmunoz@gmv.com>
2018-08-07 11:15:21 +0200

    Removed unneeded scripts

--- models/bodystate/make-links.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run the script from the build folder.
-#
-# Makes symbolic links to the configuration and data files of the 
-# tests so that they can be launched from the build folder.
-
-ln -fs $AUTOPROJ_CURRENT_ROOT/gui/vizkit3d_taste/test/config-bodystate.yml config-bodystate.yml
-ln -fs $AUTOPROJ_CURRENT_ROOT/gui/vizkit3d_taste/test/stim300.stl stim300.stl
